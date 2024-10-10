a733a9ccd84d5af44211aac48fa7b1c9e284e0e6
Uttkarsh Bharadia <uttkarsh02102001@gmail.com>
2024-10-09 23:45:25 -0400

    Added Github Workflows
    Added Github workflow file, and made necessary updates to files

--- main.tf
+++ main.tf
@@ -7,22 +7,23 @@ provider "aws" {
 module "vpc" {
   source         = "./modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
+  vpc_name       = var.vpc_name
 }
 
 # Subnets Module - This outputs the public and private subnet IDs
 module "subnets" {
-  source              = "./modules/subnet"
-  vpc_id              = module.vpc.vpc_id
-  public_subnet_cidrs = var.public_subnet_cidrs
+  source               = "./modules/subnet"
+  vpc_id               = module.vpc.vpc_id
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  availability_zones  = var.availability_zones
+  availability_zones   = var.availability_zones
 }
 
 
 # Routing Module - Pass the VPC ID and subnet IDs to the routing module
 module "routing" {
-  source              = "./modules/routing"
-  vpc_id              = module.vpc.vpc_id  # Get the VPC ID from the VPC module
-  public_subnet_ids   = module.subnets.public_subnet_ids  # Pass public subnet IDs from subnets module
-  private_subnet_ids  = module.subnets.private_subnet_ids # Pass private subnet IDs from subnets module
+  source             = "./modules/routing"
+  vpc_id             = module.vpc.vpc_id                 # Get the VPC ID from the VPC module
+  public_subnet_ids  = module.subnets.public_subnet_ids  # Pass public subnet IDs from subnets module
+  private_subnet_ids = module.subnets.private_subnet_ids # Pass private subnet IDs from subnets module
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,6 +4,6 @@ resource "aws_vpc" "my_vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "MyVPC"
+    Name = var.vpc_name
   }
 }
