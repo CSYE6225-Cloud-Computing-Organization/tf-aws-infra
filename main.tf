9b6481a86a95a8d04a3e701afa6825170f4947a0
Uttkarsh222 <60187484+Uttkarsh222@users.noreply.github.com>
2024-10-14 17:17:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,6 @@ provider "aws" {
   region  = var.AWS_REGION
 }
 
-# VPC Module - This outputs the VPC ID
-module "vpc" {
-  source         = "./modules/vpc"
-  vpc_cidr_block = var.vpc_cidr_block
-  vpc_name       = var.vpc_name
-}
 
 # Subnets Module - This outputs the public and private subnet IDs
 module "subnets" {
@@ -21,7 +15,7 @@ module "subnets" {
 
 
 # Routing Module - Pass the VPC ID and subnet IDs to the routing module
-    module "routing" {
+module "routing" {
   source             = "./modules/routing"
   vpc_id             = module.vpc.vpc_id                 # Get the VPC ID from the VPC module
   public_subnet_ids  = module.subnets.public_subnet_ids  # Pass public subnet IDs from subnets module
